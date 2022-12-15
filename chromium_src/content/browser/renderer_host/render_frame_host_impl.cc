ce72197bacd28d736c27c43f2663237486f10e41
Admin <ubuntu@ubuntu.ubuntu-domain>
2022-12-15 11:58:36 +0800

    fix: enable g_allow_injecting_javascript in android

--- chromium_src/content/browser/renderer_host/render_frame_host_impl.cc
+++ chromium_src/content/browser/renderer_host/render_frame_host_impl.cc
@@ -4,5 +4,11 @@
 #include "build/build_config.h"
 #include "src/content/browser/renderer_host/render_frame_host_impl.cc"
 #if BUILDFLAG(IS_ANDROID)
-bool g_allow_injecting_javascript = true;
+class MisesAllowInjectingJavascript {
+public:
+    MisesAllowInjectingJavascript() {
+        content::RenderFrameHost::AllowInjectingJavaScript();
+    }
+};
+static MisesAllowInjectingJavascript  s_misse_allow_injecting_javascript;
 #endif
