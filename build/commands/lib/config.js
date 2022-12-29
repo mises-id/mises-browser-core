c9f291489ee1548402bf34565e17991cadee920c
freshlife001 <>
2022-12-29 21:48:09 +0800

    fix: add android version code overide

--- build/commands/lib/config.js
+++ build/commands/lib/config.js
@@ -481,6 +481,7 @@ Config.prototype.buildArgs = function () {
     args.target_android_output_format =
       this.targetAndroidOutputFormat || (this.buildConfig === 'Release' ? 'aab' : 'apk')
     args.android_override_version_name = this.androidOverrideVersionName
+    args.android_override_version_code = this.androidOverrideVersionCode
 
     args.mises_android_developer_options_code = this.misesAndroidDeveloperOptionsCode
     args.mises_safetynet_api_key = this.misesSafetyNetApiKey
@@ -702,6 +703,9 @@ Config.prototype.update = function (options) {
     }
     if (options.android_override_version_name) {
       this.androidOverrideVersionName = options.android_override_version_name
+    }
+    if (options.android_override_version_code) {
+      this.androidOverrideVersionCode = options.android_override_version_code
     }
   }
 
