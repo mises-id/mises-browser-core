f1c57170d170fff2c86a4404bef7aa7c304a64ee
lmk <1056509662@qq.com>
2023-01-15 04:50:18 +0800

    fix: buildable on android now

---
+++ android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/MisesTabUiFeatureUtilities.java
@@ -0,0 +1,34 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+import org.chromium.chrome.browser.flags.CachedFeatureFlags;
+import org.chromium.chrome.browser.preferences.BravePreferenceKeys;
+import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
+
+public class MisesTabUiFeatureUtilities {
+    private static final String TAB_GROUP_AUTO_CREATION_PREFERENCE =
+            "Chrome.Flags.FieldTrialParamCached.TabGridLayoutAndroid:enable_tab_group_auto_creation";
+
+    @SuppressLint("VisibleForTests")
+    public static void maybeOverrideEnableTabGroupAutoCreationPreference(Context context) {
+        if (TabUiFeatureUtilities.isTabGroupsAndroidEnabled(context)) {
+            // Override it to make "Open in new tab" menu option in the context menu available if
+            // applicable.
+            SharedPreferencesManager.getInstance().writeBoolean(
+                    TAB_GROUP_AUTO_CREATION_PREFERENCE, isBraveTabGroupsEnabled());
+            CachedFeatureFlags.resetFlagsForTesting();
+        }
+    }
+
+    public static boolean isBraveTabGroupsEnabled() {
+        return SharedPreferencesManager.getInstance().readBoolean(
+                BravePreferenceKeys.BRAVE_TAB_GROUPS_ENABLED, true);
+    }
+}
