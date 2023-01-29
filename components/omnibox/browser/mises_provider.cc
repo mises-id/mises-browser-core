6e123916b99aa7325e592bd23384b21639cee751
cg917658910 <917658910@qq.com>
2023-01-29 11:13:34 +0800

    fix: web3sites match relevance

--- components/omnibox/browser/mises_provider.cc
+++ components/omnibox/browser/mises_provider.cc
@@ -76,8 +76,8 @@ void MisesProvider::DoAutocomplete(const AutocompleteInput &input){
      ACMatchClassifications description_class_ = ClassifyTermMatches(term_matches, description_.size(),
                                            ACMatchClassification::MATCH | ACMatchClassification::URL,
                                            ACMatchClassification::URL);
-      
-     //content 
+
+     //content
      std::u16string contents_ = mises_match.contents;
      if (contents_.empty()){
       contents_ = description_;
@@ -110,10 +110,11 @@ void MisesProvider::DoAutocomplete(const AutocompleteInput &input){
       if(!mises_matches[i].image_url.is_empty()){
         client_->PrefetchImage(mises_matches[i].image_url);
     }
+    mises_matches[i].relevance = 1299;
     matches_.push_back(mises_matches[i]);
   }
   mises_matches.clear();
-  NotifyListeners(true); 
+  NotifyListeners(true);
 }
 
 // static
@@ -189,7 +190,7 @@ void MisesProvider::GetTopSiteData() {
 }
 
 void MisesProvider::OnURLLoadComplete(const network::SimpleURLLoader* source,
-                                    std::unique_ptr<std::string> response_body){                              
+                                    std::unique_ptr<std::string> response_body){
     LOG(INFO) << "Cg MisesProvider::OnURLLoadComplete";
     int response_code = -1;
     if (source->ResponseInfo() &&
@@ -217,7 +218,7 @@ void MisesProvider::OnURLLoadComplete(const network::SimpleURLLoader* source,
         return;
     }
     LOG(INFO) << "Cg MisesProvider::DoAutocomplete set_new_top_sites";
-    
+
     auto data_list = json_value->GetListDeprecated();
 
     for (const auto& data : data_list) {
