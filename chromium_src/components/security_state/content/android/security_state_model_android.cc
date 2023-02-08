6ced383617ae2588282f6fd9927f76c0be722bcc
freshlife001 <talent@mises.site>
2023-02-08 16:56:49 +0800

    fix: android GetSecurityLevel

--- chromium_src/components/security_state/content/android/security_state_model_android.cc
+++ chromium_src/components/security_state/content/android/security_state_model_android.cc
@@ -1,3 +1,10 @@
+#include "mises/components/ipfs/buildflags/buildflags.h"
+#if BUILDFLAG(ENABLE_IPFS)
+#include "mises/components/ipfs/ipfs_constants.h"
+
+#include "mises/browser/net/decentralized_dns_network_delegate_helper.h"
+#endif
+
 #define JNI_SecurityStateModel_GetSecurityLevelForWebContents \
     JNI_SecurityStateModel_GetSecurityLevelForWebContents_Chromium
 #include "src/components/security_state/content/android/security_state_model_android.cc"
@@ -10,7 +17,10 @@ jint JNI_SecurityStateModel_GetSecurityLevelForWebContents(
       content::WebContents::FromJavaWebContents(jweb_contents);
   DCHECK(web_contents);
   if (web_contents) {
-    auto * vs = security_state::GetVisibleSecurityState(web_contents);
+     const auto state = security_state::GetVisibleSecurityState(web_contents);
+    if (state && decentralized_dns::ShouldHandleUrl(state->url)) {
+      return security_state::NONE;
+    }
   }
 
   return JNI_SecurityStateModel_GetSecurityLevelForWebContents_Chromium(env, jweb_contents);
