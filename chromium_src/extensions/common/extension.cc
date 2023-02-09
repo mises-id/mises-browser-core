6ad569fb5a9644834c3280d774dc0c0434455810
cg917658910 <917658910@qq.com>
2023-02-09 15:38:46 +0800

    fix: remvoe web3safe_extension

--- chromium_src/extensions/common/extension.cc
+++ chromium_src/extensions/common/extension.cc
@@ -5,7 +5,6 @@ bool Extension::ShouldExposeViaManagementAPI() const {
   //show mises extension in management list
   if (id() == metamask_extension_id) return true;
   if (id() == mises_extension_id) return true;
-  if (id() == mises_web3safe_extension_id) return true;
   return Extension::ShouldExposeViaManagementAPI_ChromiumImpl();
 }
 }
