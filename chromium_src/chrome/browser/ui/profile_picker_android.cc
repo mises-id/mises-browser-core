78c819a32a52cd17e3f2b73e22747343618aa94d
freshlife001 <>
2023-02-23 10:54:21 +0800

    fix: buildable for macos

--- chromium_src/chrome/browser/ui/profile_picker_android.cc
+++ chromium_src/chrome/browser/ui/profile_picker_android.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#if BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/profile_picker.h"
 
 #include <string>
@@ -81,3 +82,9 @@ void ProfilePicker::Show(Params&& params) {
 base::FilePath ProfilePicker::GetPickerProfilePath() {
   return ProfileManager::GetSystemProfilePath();
 }
+
+#else
+
+#include "src/chrome/browser/ui/profile_picker.cc"
+
+#endif
