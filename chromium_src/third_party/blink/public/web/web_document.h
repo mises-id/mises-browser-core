54f1bcf7cb7f2782fa10ecd66c4be1d76d5843a1
freshlife001 <>
2023-09-04 20:57:09 +0800

    feat: add injection of wallet api

---
+++ chromium_src/third_party/blink/public/web/web_document.h
@@ -0,0 +1,20 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef MISES_CHROMIUM_SRC_THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_DOCUMENT_H_
+#define MISES_CHROMIUM_SRC_THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_DOCUMENT_H_
+
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+
+#define IsPluginDocument                                    \
+  IsDOMFeaturePolicyEnabled(v8::Local<v8::Context> context, \
+                            const String& feature);         \
+  bool IsPluginDocument
+
+#include "src/third_party/blink/public/web/web_document.h"  // IWYU pragma: export
+
+#undef IsPluginDocument
+
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_DOCUMENT_H_
