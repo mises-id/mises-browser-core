d392bec60f9f09444ab5e8dd0847d76787d8b0a4
freshlife001 <>
2023-10-05 23:18:06 +0800

    fix: free name

--- third_party/freename/generate.sh
+++ third_party/freename/generate.sh
@@ -1,16 +1,10 @@
-# curl --location 'https://api.freename.io/item/get-all-tlds' \
+# curl --location 'https://api.freename.io/item/allTld' \
 # -o freename_tld.json \
-# --header 'Content-Type: application/json' \
-# --data '{
-#     "filters": [],
-#     "limit": 10000,
-#     "offset": 0,
-#     "order": "date_DESC"
-# }'
+# --header 'Content-Type: application/json' 
 
 
-cat freename_tld.json | jq -c '.data.elements | select( all(.[]; .existingDomainsCount > 0))|=sort_by(.name) |to_entries[]|.value.name[1:]' --raw-output > freename_tld.dat
-../../src/out/Debug_arm64/tld_cleanup
+cat freename_tld.json | jq -c ' select( all(.[]; .existingDomainsCount > 0))|=sort_by(.name) |to_entries[]|.value.name[1:]' --raw-output > freename_tld.dat
+../../src/out/Debug_arm64/freename_tld_cleanup
 python fn_make_dafsa.py --reverse freename_tld.gperf freename_tld_names-reversed-inc.cc
 
 python fn_make_dafsa.py --binary --reverse freename_tld.gperf freename_tld_names-reversed-inc.dat
