fa1e60d92b5656de8dfb82cca9da65352ecee38d
freshlife001 <talent@mises.site>
2024-11-14 18:06:22 +0800

    fix: legacy features

--- components/ipfs/resources/ipfs_interstitial.js
+++ components/ipfs/resources/ipfs_interstitial.js
@@ -3,22 +3,31 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at https://mozilla.org/MPL/2.0/.
 
-function setupEvents() {
-  $('body').classList.add('ipfs');
-  $('icon').classList.add('icon');
+import { HIDDEN_CLASS, SecurityInterstitialCommandId, sendCommand } from 'chrome://interstitials/common/resources/interstitial_common.js';
 
-  $('primary-button').addEventListener('click', function() {
+
+function setupEvents() {
+  const body = document.querySelector('#body');
+  body.classList.add('ipfs');
+  const icon = document.querySelector('#icon');
+  icon.classList.add('icon');
+
+  const primaryButton = document.querySelector('#primary-button');
+  primaryButton.addEventListener('click', function() {
     sendCommand(SecurityInterstitialCommandId.CMD_PROCEED);
   });
 
-  $('main-content').classList.remove(HIDDEN_CLASS);
+  const mainContent = document.querySelector('#main-content');
+  mainContent.classList.remove(HIDDEN_CLASS);
 
-  $('details-button').addEventListener('click', function(event) {
-    const hiddenDetails = $('details').classList.toggle(HIDDEN_CLASS);
-    $('details-button').innerText = hiddenDetails ?
-        loadTimeData.getString('openDetails') :
-        loadTimeData.getString('closeDetails');
+  const detailButton = document.querySelector('details-button')
+  detailButton.addEventListener('click', function(event) {
+    const details = document.querySelector('#main-content');
+    detailButton.innerText = hiddenDetails ?
+      loadTimeData.getString('openDetails') :
+      loadTimeData.getString('closeDetails');
   });
+
 }
 
 document.addEventListener('DOMContentLoaded', setupEvents);
